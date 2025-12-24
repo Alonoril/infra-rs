d985ef0854dd03abedd8b368eb0383bb7fd39895
Alon <miki20141110@gmail.com>
2025-12-24 17:05:09 +0800

    #new: schema value/key codec

--- rksdb/src/codec.rs
+++ rksdb/src/codec.rs
@@ -1,3 +1,55 @@
+///pub struct SchemaKey;
+///
+/// impl SchemaKey {
+/// 	pub fn encode(&self) -> AppResult<Vec<u8>> {
+/// 		Ok(vec![])
+/// 	}
+///
+/// 	pub fn decode(bytes: &[u8]) -> AppResult<SchemaKey> {
+/// 		Ok(SchemaKey)
+/// 	}
+/// }
+#[macro_export]
+macro_rules! impl_schema_key_codec {
+	($schema_type:ty, $key_type:ty) => {
+		impl $crate::schemadb::schema::KeyCodec<$schema_type> for $key_type {
+			fn encode_key(&self) -> base_infra::result::AppResult<Vec<u8>> {
+				self.encode()
+			}
+
+			fn decode_key(data: &[u8]) -> base_infra::result::AppResult<Self> {
+				Self::decode(data)
+			}
+		}
+	};
+}
+
+///pub struct SchemaValue;
+///
+/// impl SchemaValue {
+/// 	pub fn encode(&self) -> AppResult<Vec<u8>> {
+/// 		Ok(vec![])
+/// 	}
+///
+/// 	pub fn decode(bytes: &[u8]) -> AppResult<SchemaValue> {
+/// 		Ok(SchemaValue)
+/// 	}
+/// }
+#[macro_export]
+macro_rules! impl_schema_value_codec {
+	($schema_type:ty, $value_type:ty) => {
+		impl $crate::schemadb::schema::ValueCodec<$schema_type> for $value_type {
+			fn encode_value(&self) -> base_infra::result::AppResult<Vec<u8>> {
+				self.encode()
+			}
+
+			fn decode_value(data: &[u8]) -> base_infra::result::AppResult<Self> {
+				Self::decode(data)
+			}
+		}
+	};
+}
+
 /// A macro to generate the `KeyCodec` and `ValueCodec` implementations for a given schema type  with bincode.
 #[macro_export]
 macro_rules! impl_schema_bin_codec {
@@ -79,31 +131,31 @@ macro_rules! impl_schema_bcs_codec {
 /// A macro to generate the `KeyCodec` implementation for a given schema type.
 #[macro_export]
 macro_rules! impl_schema_key_bcs_codec {
-    ($schema_type:ty, $key_type:ty) => {
-        impl $crate::schemadb::schema::KeyCodec<$schema_type> for $key_type {
-            fn encode_key(&self) -> anyhow::Result<Vec<u8>> {
-                bcs::to_bytes(self).map_err(Into::into)
-            }
+	($schema_type:ty, $key_type:ty) => {
+		impl $crate::schemadb::schema::KeyCodec<$schema_type> for $key_type {
+			fn encode_key(&self) -> anyhow::Result<Vec<u8>> {
+				bcs::to_bytes(self).map_err(Into::into)
+			}
 
-            fn decode_key(data: &[u8]) -> anyhow::Result<Self> {
-                bcs::from_bytes(data).map_err(Into::into)
-            }
-        }
-    };
+			fn decode_key(data: &[u8]) -> anyhow::Result<Self> {
+				bcs::from_bytes(data).map_err(Into::into)
+			}
+		}
+	};
 }
 
 /// A macro to generate the `ValueCodec` implementation for a given schema type.
 #[macro_export]
 macro_rules! impl_schema_value_bcs_codec {
-    ($schema_type:ty, $value_type:ty) => {
-        impl $crate::schemadb::schema::ValueCodec<$schema_type> for $value_type {
-            fn encode_value(&self) -> anyhow::Result<Vec<u8>> {
-                bcs::to_bytes(self).map_err(Into::into)
-            }
+	($schema_type:ty, $value_type:ty) => {
+		impl $crate::schemadb::schema::ValueCodec<$schema_type> for $value_type {
+			fn encode_value(&self) -> anyhow::Result<Vec<u8>> {
+				bcs::to_bytes(self).map_err(Into::into)
+			}
 
-            fn decode_value(data: &[u8]) -> anyhow::Result<Self> {
-                bcs::from_bytes(data).map_err(Into::into)
-            }
-        }
-    };
+			fn decode_value(data: &[u8]) -> anyhow::Result<Self> {
+				bcs::from_bytes(data).map_err(Into::into)
+			}
+		}
+	};
 }
